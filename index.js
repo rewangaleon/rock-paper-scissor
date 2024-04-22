1df8693e6838147f2c1d57c96f7b62004b3e39ff
James <jun1718www@naver.com>
2024-04-22 16:50:37 +0800

    Input of pc and player ok. Result of duel is ok getComputerChoice is ok playRound is ok getPlayer is ok

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 function getComputerChoice(){
-    return "rock";
+    let choice = [`rock`, `paper`, `scissor`];
+    let randomNumber = Math.floor(Math.random() * 3);
+    // console.log(choice[randomNumber]);
+    return choice[randomNumber];
 }
 
-
 function playRound(playerSelection, computerSelection) {
-    return(`${playerSelection} and ${computerSelection}`);
-  }
+    playerSelection = playerSelection.toLowerCase();
+    let result = 0;
+
+    if(playerSelection==computerSelection){
+        result = "The round is a tie!"
+    }
+
+    if(playerSelection==`scissor`){
+        if(computerSelection==`rock`) result = "The Player lose!"
+        if(computerSelection==`paper`) result = "The Player win!"
+    }
+
+    if(playerSelection==`rock`){
+        if(computerSelection==`paper`) result = "The Player lose!"
+        if(computerSelection==`scissor`) result = "The Player win!"
+    }
+
+    if(playerSelection==`paper`){
+        if(computerSelection==`scissor`) result = "The Player lose!"
+        if(computerSelection==`rock`) result = "The Player win!"
+    }
+
+    return(`Player Choice = ${playerSelection}, Computer Choice = ${computerSelection}. ${result}`);
+}
   
-  const playerSelection = "paper";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+const playerSelection = prompt("Please enter your choice:");
+let computerSelection = 0;
+let result = 0;
+
+computerSelection = getComputerChoice();
+result = playRound(playerSelection, computerSelection);
+console.log(result);
+
+// document.getElementById("result1").innerHTML = result;
+let paragraphElement = document.getElementById("result1");
+paragraphElement.textContent = result;
   
